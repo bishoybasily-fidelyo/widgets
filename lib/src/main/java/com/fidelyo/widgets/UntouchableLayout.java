5c6e764b2efb52da5d25da1132b3bf35d25d79cb
bishoybasily <bishoybasily@gmail.com>
2018-05-13 07:22:27 +0200

    cleaning

--- lib/src/main/java/com/fidelyo/widgets/UntouchableLayout.java
+++ lib/src/main/java/com/fidelyo/widgets/UntouchableLayout.java
@@ -1,6 +1,8 @@
 package com.fidelyo.widgets;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.RelativeLayout;
@@ -23,6 +25,7 @@ public class UntouchableLayout extends RelativeLayout {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     public UntouchableLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
